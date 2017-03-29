af6831a939e460d86f4b3e196f03b13ed30f68c6
Roland Tanglao <roland@rolandtanglao.com>
2017-03-29 23:40:15 +0200

    add four retries

--- test6000-redirects.rb
+++ test6000-redirects.rb
@@ -22,16 +22,32 @@ Ccsv.foreach(ARGV[0]) do |values|
   from_uri= URI(fromuri)
   touri = values[6] #column G
   to_uri = URI(touri)
-
-  Net::HTTP.start(from_uri.host, from_uri.port,
-                  :use_ssl => from_uri.scheme == 'https') do |http|
-    request = Net::HTTP::Get.new from_uri.request_uri
-    request.basic_auth userid, password
-    response = http.request request # Net::HTTPResponse object
-    if response['location'] == touri
-      printf("PASS,%d,%s,%s\n", response.code, fromuri, touri)
+  try_count = 0
+  begin 
+    Net::HTTP.start(from_uri.host, from_uri.port,
+                    :use_ssl => from_uri.scheme == 'https') do |http|
+      request = Net::HTTP::Get.new from_uri.request_uri
+      request.basic_auth userid, password
+      response = http.request request # Net::HTTPResponse object
+      response_uri = response['location']
+      if  response_uri == touri
+        printf("PASS,%d,%s,%s\n", response.code, fromuri, touri)
+      else
+        printf("FAIL,%d,%s,%s\n", response.code, fromuri, response_uri)
+      end
+    end
+  rescue Errno::ECONNRESET
+    try_count += 1
+    if try_count < 4
+      $stderr.printf("Errno::ECONNRESET exception, retry:%d\n",\
+                     try_count)
+      sleep(10)
+      retry
     else
-      printf("FAIL,%d,%s,%s\n", response.code, fromuri, touri)
+      $stderr.printf("Curl:ConnectionFailedError exception, retrying FAILED\n")
+      raise e
     end
   end
 end
+  
+    
