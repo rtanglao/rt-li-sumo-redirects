f56968b5f7e317bfefcbcfaa355428bde71a1a11
Roland Tanglao <roland@rolandtanglao.com>
2017-04-03 14:50:35 +0200

    handle 2nd redirect

--- test6000-redirects.rb
+++ test6000-redirects.rb
@@ -21,6 +21,7 @@ Ccsv.foreach(ARGV[0]) do |values|
   fromuri = values[3].gsub("support.mozilla.org", "support-stage.allizom.org") # column D
   from_uri= URI(fromuri)
   touri = values[6] #column G
+  touri = touri
   to_uri = URI(touri)
   try_count = 0
   begin 
@@ -29,7 +30,32 @@ Ccsv.foreach(ARGV[0]) do |values|
       request = Net::HTTP::Get.new from_uri.request_uri
       request.basic_auth userid, password
       response = http.request request # Net::HTTPResponse object
-      response_uri = response['location']
+      #pp response
+      #pp response.header
+      #pp response['location']
+      #pp response.code
+      if response.code == "301"
+        #puts("in first 301")
+        from_uri = URI.parse(response['location'])
+        Net::HTTP.start(from_uri.host, from_uri.port,
+                    :use_ssl => from_uri.scheme == 'https') do |http|
+          request = Net::HTTP::Get.new from_uri.request_uri
+          request.basic_auth userid, password
+          response = http.request request # Net::HTTPResponse object
+          #pp response
+          #pp response.header
+          #pp response['location']
+        end
+        if response.code == "301"
+          #puts("in 2nd 301")
+          #pp from_uri
+          #pp response['location']
+          response_uri = "https://support.mozilla.org" + response['location']
+          #pp response_uri
+        else
+          #puts("Did NOT get 2nd 301")
+        end
+      end        
       if  response_uri == touri
         printf("PASS,%d,%s,%s\n", response.code, fromuri, touri)
       else
