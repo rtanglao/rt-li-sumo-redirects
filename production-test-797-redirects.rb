89f93b86a56438edd1a924aeb7733b7b0a0ed68d
Roland Tanglao <roland@rolandtanglao.com>
2017-04-08 04:46:23 -0700

    add routine to test 797 redirects

---
+++ production-test-797-redirects.rb
@@ -0,0 +1,74 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+# http://stackoverflow.com/questions/13822555/how-to-do-basic-authentication-over-https-in-ruby
+# see also: rest-client
+
+require 'net/http'
+require 'parseconfig'
+require 'pp'
+require 'ccsv'
+require 'openssl'
+
+                        
+header = true
+row_number = 0
+Ccsv.foreach(ARGV[0]) do |columns|
+  row_number += 1
+  if header == true
+    header = false
+    next
+  end
+  fromuri = columns[0] # column A
+  from_uri= URI(fromuri)
+  touri = columns[1] #column B
+  to_uri = URI(touri)
+  try_count = 0
+  begin 
+    Net::HTTP.start(from_uri.host, from_uri.port,
+                    :use_ssl => from_uri.scheme == 'https',
+                    :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request = Net::HTTP::Get.new from_uri.request_uri
+      response = http.request request # Net::HTTPResponse object
+      response_uri = ""
+      if response.code == "301"
+        $stderr.puts("in first 301")
+        $stderr.puts response['location']
+        $stderr.puts touri
+        from_uri = URI.parse(response['location'])
+        Net::HTTP.start(from_uri.host, from_uri.port,
+                        :use_ssl => from_uri.scheme == 'https') do |http|
+          request = Net::HTTP::Get.new from_uri.request_uri
+          response = http.request request # Net::HTTPResponse object
+          response_uri = response['location']
+          $stderr.printf("2nd response uri:%s\n", response['location'])
+          response_uri = "https://support.mozilla.org" + response_uri
+        end
+      end        
+      if  response_uri == touri
+        printf("PASS,row:%d,code:%d,FROM:%s,EXPECTED:%s,ACTUAL:%s\n",\
+               row_number,response.code,fromuri, touri,response_uri)
+      else
+        printf(
+          "**FAIL**,row:%d,code:%d,FROM:%s,EXPECTED:%s,ACTUAL:**%s**\n",\
+          row_number,response.code,fromuri, touri,response_uri)
+      end
+    end
+    rescue Errno::ECONNRESET, Errno::ECONNREFUSED,
+           Net::ReadTimeout, Net::OpenTimeout,
+           SocketError => e
+    try_count += 1
+    if try_count < 6
+      $stderr.printf("%s exception, message:%s, retry:%d\n",\
+                     e.class, e.message, try_count)
+      sleep(10)
+      retry
+    else
+      $stderr.printf("%s exception, message:%s, RETRY FAILED\n",\
+                     e.class, e.message)
+      raise e
+    end
+  end
+end
+  
+    
