7c67965d75f72f2204b4968cce77d7a5e31037e3
Roland Tanglao <roland@rolandtanglao.com>
2017-04-08 04:56:42 -0700

    fix logging

--- production-test-797-redirects.rb
+++ production-test-797-redirects.rb
@@ -21,6 +21,7 @@ Ccsv.foreach(ARGV[0]) do |columns|
   fromuri = columns[0] # column A
   from_uri= URI(fromuri)
   touri = columns[1] #column B
+  guid_str = touri[touri.rindex("/") + 1, touri.length - 1]
   to_uri = URI(touri)
   try_count = 0
   begin 
@@ -49,9 +50,11 @@ Ccsv.foreach(ARGV[0]) do |columns|
         printf("PASS,row:%d,code:%d,FROM:%s,EXPECTED:%s,ACTUAL:%s\n",\
                row_number,response.code,fromuri, touri,response_uri)
       else
+        actual_guid_str = response_uri[response_uri.rindex("/") + 1,
+                                response_uri.length - 1]
         printf(
-          "**FAIL**,row:%d,code:%d,FROM:%s,EXPECTED:%s,ACTUAL:**%s**\n",\
-          row_number,response.code,fromuri, touri,response_uri)
+          "**FAIL**,row:%d,code:%d,FROM:%s,EXPECTED:%s,ACTUAL:**%s**,EXPECTEDid:%s, ACTUALid:**%s**\n",\
+          row_number,response.code,fromuri, touri,response_uri, guid_str, actual_guid_str)
       end
     end
     rescue Errno::ECONNRESET, Errno::ECONNREFUSED,
