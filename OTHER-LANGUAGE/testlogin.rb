cefc79ddb2881eba9251c6b86f154710f2592542
Roland Tanglao <roland@rolandtanglao.com>
2017-04-09 09:28:44 -0700

    print links

--- OTHER-LANGUAGE/testlogin.rb
+++ OTHER-LANGUAGE/testlogin.rb
@@ -7,8 +7,6 @@ require 'pp'
 stage_config = ParseConfig.new('user.conf').params
 userid = stage_config['userid']
 password = stage_config['password']
-puts userid
-puts password
 
 # http://merowing.info/2015/01/extracting-data-from-websites-without-api/
 mechanize = Mechanize.new
@@ -21,6 +19,10 @@ form.field_with(id: 'lia-password').value = password
 # 3
 
 page = form.submit
-pp page
+#pp page
 tkb_page = mechanize.get 'https://support.mozilla.org/t5/Manage-preferences-and-add-ons/How-to-change-your-default-browser-in-Windows-10/ta-p/35222'
-pp tkb_page.css('div.tkb-other-language-links')
+puts "1"
+pp tkb_page.css('div.tkb-other-language-link:nth-child(1) > a:nth-child(1)/@href').first.value
+puts "2"
+pp tkb_page.css('div.tkb-other-language-link:nth-child(2) > a:nth-child(1)/@href').first.value
+
