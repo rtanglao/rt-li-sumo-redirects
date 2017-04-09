8844a8684b46fcfaf691aa143075341e724dd42a
Roland Tanglao <roland@rolandtanglao.com>
2017-04-09 10:41:42 -0700

    loop through all other languages

--- OTHER-LANGUAGE/testlogin.rb
+++ OTHER-LANGUAGE/testlogin.rb
@@ -34,3 +34,8 @@ pp tkb_page.css('div.tkb-other-language-link:nth-child(1) > a:nth-child(1)').chi
 puts "2"
 pp tkb_page.css('div.tkb-other-language-link:nth-child(2) > a:nth-child(1)/@href').first.value
 
+puts "count"
+
+tkb_page.css('div.tkb-other-language-link').each do |l|
+   pp l
+end
